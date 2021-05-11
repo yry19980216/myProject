82fa01a435ea42fdc1acb2d5dd13364179b32b9e
zhangsan <zhangsan@163.com>
2021-05-12 00:59:42 +0800

    新修改后的，重新提交啦

--- src/com/yry/myproject/controller/NewClass.java
+++ src/com/yry/myproject/controller/NewClass.java
@@ -1,7 +1,15 @@
 package com.yry.myproject.controller;
 
+import java.util.Scanner;
+
 public class NewClass {
     public static void main(String[] args) {
+
         System.out.println("");
+
+        //以下为 对github上下载的原项目进行修改
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i);
     }
 }
